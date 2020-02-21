c3627c1742dd07599648a8962ff163975e519d3c
Julio Midence <jmidence@lcs.on.ca>
2020-02-20 19:51:01 -0500

    Take the code Liam

--- beggarThyNeighbour/main.swift
+++ beggarThyNeighbour/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+//player structure
+class Player {
+    var name : String
+    var hand : Hand
+    var numberOfPotsTaken = 0
+    var numberOfGamesWon = 0
+    
+    init(playerName: String) {
+        self.name = playerName
+        
+    }
+    //reset the statistics for the player
+    func resetHandStats() {
+        numberOfPotsTaken = 0
+    }
+}
 
+//actual structure for the game
+struct beggarThyNeigbourGame {
+    
+    var player1: Player
+    var player2: Player
+    
+    var gameDeck = Deck()
+    
+    mutating func giveInitialCardsToPlayers() {
+        player1.hand.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
+        player2.hand.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
+    }
+    
+    func switchWhoIsOnOffence() {
+        
+    }
+    
+}
