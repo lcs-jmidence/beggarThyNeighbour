823f823312594d98d208a0f1d582536d562463d3
Julio Midence <jmidence@lcs.on.ca>
2020-02-20 20:16:22 -0500

    Take the code at the moment, Liam

--- beggarThyNeighbour/main.swift
+++ beggarThyNeighbour/main.swift
@@ -8,38 +8,76 @@
 
 import Foundation
 
-//player structure
-class Player {
-    var name : String
-    var hand : Hand
-    var numberOfPotsTaken = 0
-    var numberOfGamesWon = 0
-    
-    init(playerName: String) {
-        self.name = playerName
-        
-    }
-    //reset the statistics for the player
-    func resetHandStats() {
-        numberOfPotsTaken = 0
-    }
-}
+
 
 //actual structure for the game
-struct beggarThyNeigbourGame {
+struct BeggarThyNeigbourGame {
     
-    var player1: Player
-    var player2: Player
+    //create two players to play
+    var player1: Hand = Hand(description: "player1")
+    var player2: Hand = Hand(description: "player2")
     
+    //create offence and defence indicators
+    var offence: Hand
+    var defence: Hand
+    
+    //create a pot array where the cards will be placed in the middle
+    var potOfGold: [Card] = []
+    
+    //create the deck to be used for the game
     var gameDeck = Deck()
     
-    mutating func giveInitialCardsToPlayers() {
-        player1.hand.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
-        player2.hand.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
+    var gameIsOngoing = true
+    
+    
+    //initialize the indicators to be player 1 and 2
+    init() {
+        
+        //assign offence and defence
+        offence = player1
+        defence = player2
+        
+        //run the play function
+        play()
     }
     
-    func switchWhoIsOnOffence() {
+    
+    //deal out the cards
+    mutating func giveInitialCardsToPlayers() {
+        player1.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
+        player2.cards.append(contentsOf: gameDeck.randomlyDealOut(thisManyCards: 26)!)
+    }
+    
+    //switch who is on offence
+    mutating func switchWhoIsOnOffence() {
+        //Switch the offence if it is player1
+        if offence === player1 {
+            offence = player2
+            defence = player1
+            // Switch the offence if it is player 2
+        } else {
+            offence = player1
+            defence = player2
+        }
+    }
+    
+    mutating func gameIsOver(winnerIs: String) {
+        gameIsOngoing = false
+        print("The winner is \(winnerIs)!")
+    }
+    
+    mutating func checkIfSomeoneHasLost() {
+        if player1.cards.count <= 0 {
+            gameIsOver(winnerIs: "player two")
+        } else if player2.cards.count <= 0 {
+            gameIsOver(winnerIs: "player one")
+        }
+    }
+    
+    //play the game
+    func play() {
         
     }
     
 }
+
