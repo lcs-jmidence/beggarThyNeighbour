bdb2e2af383345b96a0d92c79a4ac4669e47b61b
Julio Midence <jmidence@lcs.on.ca>
2020-02-20 19:26:46 -0500

    Initial Commit

---
+++ beggarThyNeighbour/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  beggarThyNeighbour
+//
+//  Created by Midence Rosario, Julio on 2020-02-20.
+//  Copyright © 2020 Julio Midence. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
